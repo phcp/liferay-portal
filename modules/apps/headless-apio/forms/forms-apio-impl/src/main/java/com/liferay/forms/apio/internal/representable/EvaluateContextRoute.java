a611d3684f91c8b011e29e7e216ac3b2544c3515
Javier Gamarra <nhpatt@gmail.com>
2018-05-16 07:40:41 +0000

    evaluatecontext

---
+++ modules/apps/headless-apio/forms/forms-apio-impl/src/main/java/com/liferay/forms/apio/internal/representable/EvaluateContextRoute.java
@@ -0,0 +1,10 @@
+package com.liferay.forms.apio.internal.representable;
+
+import com.liferay.apio.architect.customactions.PostRoute;
+
+public class EvaluateContextRoute extends PostRoute {
+	@Override
+	public String getName() {
+		return "evaluateContext";
+	}
+}
