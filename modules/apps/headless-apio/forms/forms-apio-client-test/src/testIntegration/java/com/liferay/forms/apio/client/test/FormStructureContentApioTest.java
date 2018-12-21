01b8b2f89b4525082354df25db1592f4f7586e2e
Paulo Cruz <paulo.cruz@liferay.com>
2018-12-21 11:48:59 -0300

    WIP

---
+++ modules/apps/headless-apio/forms/forms-apio-client-test/src/testIntegration/java/com/liferay/forms/apio/client/test/FormStructureContentApioTest.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.forms.apio.client.test;
+
+import static io.restassured.RestAssured.withArgs;
+
+import com.liferay.forms.apio.client.test.activator.FormStructureApioTestBundleActivator;
+import com.liferay.oauth2.provider.test.util.OAuth2ProviderTestUtil;
+import com.liferay.portal.apio.test.util.ApioClientBuilder;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.Archive;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Paulo Cruz
+ */
+@RunAsClient
+@RunWith(Arquillian.class)
+public class FormStructureContentApioTest {
+
+	@Deployment
+	public static Archive<?> getArchive() throws Exception {
+		return OAuth2ProviderTestUtil.getArchive(
+			FormStructureApioTestBundleActivator.class);
+	}
+
+	@Before
+	public void setUp() throws MalformedURLException {
+		_rootEndpointURL = new URL(_url, "/o/api");
+	}
+
+	@Test
+	public void testTextFieldDataTypeIsDisplayed() {
+		_fieldPropertyMatches(
+			"MyTextField", "dataType", Matchers.equalTo("string"));
+	}
+
+	@Test
+	public void testTextFieldInputControlIsDisplayed() {
+		_fieldPropertyMatches(
+			"MyTextField", "inputControl", Matchers.equalTo("text"));
+	}
+
+	private void _fieldPropertyMatches(
+		String fieldName, String fieldPropertyName, Matcher matcher) {
+
+		String href = ApioClientBuilder.given(
+		).basicAuth(
+			"test@liferay.com", "test"
+		).header(
+			"Accept", "application/hal+json"
+		).header(
+			"Accept-Language", "en-US"
+		).when(
+		).get(
+			_rootEndpointURL.toExternalForm()
+		).follow(
+			"_links.content-space.href"
+		).then(
+		).extract(
+		).path(
+			"_embedded.ContentSpace.find {it.name == '%s'}._links." +
+				"formStructures.href",
+			FormStructureApioTestBundleActivator.SITE_NAME
+		);
+
+		ApioClientBuilder.given(
+		).basicAuth(
+			"test@liferay.com", "test"
+		).header(
+			"Accept", "application/hal+json"
+		).header(
+			"Accept-Language", "en-US"
+		).when(
+		).get(
+			href
+		).then(
+		).log(
+		).ifError(
+		).statusCode(
+			200
+		).body(
+			"_embedded.Structure[0]._embedded.formPages._embedded[0]." +
+				"_embedded.fields._embedded.find {it.name == '%s'}.%s",
+			withArgs(fieldName, fieldPropertyName), matcher
+		);
+	}
+
+	private URL _rootEndpointURL;
+
+	@ArquillianResource
+	private URL _url;
+
+}
